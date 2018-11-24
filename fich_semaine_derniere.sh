7dec027e7a57ec5184d93d960f176e6650b67a15
Jonathan Tricard <tricardj@hotmail.fr>
2018-11-24 16:46:17 +0100

    Ajout de fich_semaine_derniere.sh.

---
+++ fich_semaine_derniere.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+JOUR=$(date +%d)
+MOIS=$(date +%B)
+ANNEE=$(date +%Y)
+read -p "Comment t'appelles-tu?" NOM
+echo "Bonjour $NOM, bienvenu sur mon projet, nous somme le $JOUR $MOIS $ANNEE et tu es connecté depuis ton compte $USERNAME."
+read -p "Quel répertoire t'intéresse camarade?" REP
+echo "As-tu bien fait attention aux consignes du README en écrivant le répertoire $REP?"
+
